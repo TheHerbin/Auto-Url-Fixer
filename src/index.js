00957411a62b5670e91c4bb23bfb829ba5aa35a5
Theo Herbin <theo.herbin59710@outlook.fr>
2024-02-07 14:00:13 +0100

    finalized bot , fully working

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const {Client,IntentsBitField, userMention} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+client.on('ready',(c)=>{
+    console.log(`The bot named ${c.user.tag} is now ready.`)
+})
+
+client.on('messageCreate',(message) => {
+    if(message.author.bot){
+        return
+    }
+
+    if(message.content === "test"){
+
+        message.reply("Voici un test bien réussi !")
+    }
+    const xString = "x.com";
+    const vxTwitterString = "vxtwitter.com";
+    let fixedTwitterString = "";
+    if(message.content.includes(xString)){
+        fixedTwitterString = message.content.replace(xString, vxTwitterString);
+        const channel = message.channelId
+        message.delete(1000);
+        message.channel.send(userMention(message.author.id)+", "+"I corrected your link so it can be embedded : "+fixedTwitterString)
+        
+    }
+
+})
+client.login(process.env.TOKEN)
